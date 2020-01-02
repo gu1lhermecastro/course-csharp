b633c80fbf2390d670e52810de7ddb47db98c0a9
Guilherme Castro <guilhermecastro.developer@gmail.com>
2020-01-02 00:40:21 -0300

    fixation-exercise 1

---
+++ fixation-exercise/fixation-exercise/BankAccount.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace fixation_exercise
+{
+    class BankAccount
+    {
+        public int NumberAccount { get; set; }
+        public string Holder { get; set; }
+        public double Balance { get; private set; }
+        //TODO: Improve implements tax per withdrawal
+        private double TaxPerWithdrawal = 5.0;
+
+
+        public BankAccount(int NumberAccount, string Holder)
+        {
+            this.NumberAccount = NumberAccount;
+            this.Holder = Holder;
+        }
+
+        public BankAccount(int NumberAccount, string Holder, double Balance) : this (NumberAccount, Holder)
+        {
+            AddCharge(Balance);
+        }
+
+        public void AddCharge(double DepositAmount)
+        {
+            this.Balance += DepositAmount;
+        }
+        public void BankWithdrawal(double WithdrawalAmount)
+        {
+            this.Balance -= (WithdrawalAmount + TaxPerWithdrawal);
+        }
+
+        public override string ToString()
+        {
+            return $"\nDados da conta atualizados:\nConta {NumberAccount}, Titular: {Holder}, Saldo: {Balance}";
+        }
+    }
+}
---
+++ fixation-exercise/fixation-exercise/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace fixation_exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Entre o número da conta: ");
+            int numberAccount = int.Parse(Console.ReadLine());
+
+            Console.Write("Entre o titular da conta: ");
+            string holder = Console.ReadLine();
+
+            Console.Write("Haverá depósito inicial (s/n)? ");
+            char option = char.Parse(Console.ReadLine());
+
+            BankAccount bankAccount;
+
+            if (option == 's')
+            {
+                Console.Write("Entre o valor de depósito inicial: ");
+                double initialDepositAmount = double.Parse(Console.ReadLine());
+                bankAccount = new BankAccount(numberAccount, holder, initialDepositAmount);
+            }
+            else
+            {
+                bankAccount = new BankAccount(numberAccount, holder);
+            }
+
+            Console.Write(bankAccount.ToString());
+
+            Console.Write("Entre um valor para depósito: ");
+            double depositAmount = double.Parse(Console.ReadLine());
+            bankAccount.AddCharge(depositAmount);
+            Console.Write(bankAccount.ToString());
+
+            Console.Write("Entre um valor para saque: ");
+            double withdrawalAmount = double.Parse(Console.ReadLine());
+            bankAccount.BankWithdrawal(withdrawalAmount);
+            Console.Write(bankAccount.ToString());
+        }
+    }
+}
